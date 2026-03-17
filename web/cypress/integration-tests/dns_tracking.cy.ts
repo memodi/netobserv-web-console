netobserv/network-observability-console-plugin

migrating frontend integration tests to upstream

web/cypress/integration-tests/dns_tracking.cy.ts
@@ -1,0 +1,98 @@
+import { colSelectors, filterSelectors, netflowPage, overviewSelectors, querySumSelectors } from "@views/netflow-page"
+import { Operator, project } from "@views/netobserv"
+
+describe('(OCP-67087 Network_Observability) DNSTracking test', { tags: ['Network_Observability'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogin(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+
+        Operator.install()
+        cy.checkStorageClass(this)
+        Operator.createFlowcollector("DNSTracking")
+    })
+
+    beforeEach('any DNSTracking test', function () {
+        netflowPage.visit()
+    })
+
+    it("(OCP-67087, aramesha, Network_Observability) Verify DNSTracking panels and Query Summary", function () {
+        // verify default DNSTracking panels are visible
+        cy.checkPanel(overviewSelectors.defaultDNSTrackingPanels)
+        cy.checkPanelsNum(6);
+
+        // open panels modal and verify all relevant panels are listed
+        cy.openPanelsModal()
+        cy.checkPopupItems(overviewSelectors.panelsModal, overviewSelectors.manageDNSTrackingPanelsList);
+
+        // select all panels and verify they are rendered
+        cy.get(overviewSelectors.panelsModal).contains('Select all').click();
+        cy.get(overviewSelectors.panelsModal).contains('Save').click();
+        netflowPage.waitForLokiQuery()
+        cy.checkPanelsNum(11);
+
+        netflowPage.waitForLokiQuery()
+        cy.checkPanel(overviewSelectors.allDNSTrackingPanels)
+
+        // restore default panels and verify they are visible
+        cy.openPanelsModal();
+        cy.byTestID(overviewSelectors.resetDefault).click().byTestID(overviewSelectors.save).click()
+        netflowPage.waitForLokiQuery()
+        cy.checkPanel(overviewSelectors.defaultDNSTrackingPanels)
+        cy.checkPanelsNum(6);
+
+        // verify Query Summary stats for DNSTracking
+        cy.get(querySumSelectors.dnsAvg).should('exist').then(DNSAvg => {
+            cy.checkQuerySummary(DNSAvg)
+        })
+    })
+
+    it("(OCP-67087, aramesha) Validate DNSTracking columns and DNSName", function () {
+        cy.get('#tabs-container li:nth-child(2)').click()
+        cy.byTestID("table-composable").should('exist')
+        netflowPage.stopAutoRefresh()
+
+        // verify default DNS columns: DNS Latency and DNS Response Code
+        cy.byTestID('table-composable').should('exist').within(() => {
+            cy.get(colSelectors.DNSLatency).should('exist')
+            cy.get(colSelectors.DNSResponseCode).should('exist')
+        })
+
+        // add filter for dst_ns=netobserv and DNSName=loki
+        const dns_name = "loki.netobserv.svc.cluster"
+        cy.get(filterSelectors.filterInput).type("dst_namespace=" + project + '{enter}')
+        cy.get(filterSelectors.filterInput).type("dns_name=" + dns_name + '{enter}')
+
+        // select DNS Id and DNS Error columns
+        cy.openColumnsModal().then(col => {
+            cy.get(colSelectors.columnsModal).should('be.visible')
+            cy.get('#DNSId').check()
+            cy.get('#DNSErrNo').check()
+            cy.get('#DNSName').check()
+            cy.byTestID(colSelectors.save).click()
+        })
+
+        // verify they are visible in table view
+        cy.byTestID('table-composable').should('exist').within(() => {
+            cy.get(colSelectors.DNSId).should('exist')
+            cy.get(colSelectors.DNSError).should('exist')
+            cy.get(colSelectors.DNSName).should('exist')
+        })
+
+        // Verify DNSName column for all rows
+        cy.get('[data-test-td-column-id="DNSName"]').each((td) => {
+            expect(td).to.contain(`${dns_name}`)
+        })
+
+        netflowPage.clearAllFilters()
+    })
+
+    afterEach("test", function () {
+        netflowPage.resetClearFilters()
+    })
+
+    after("Delete flowcollector and DNS pods", function () {
+        Operator.deleteFlowCollector()
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+    })
+})

web/cypress/integration-tests/flowRTT.cy.ts
@@ -1,0 +1,68 @@
+import { Operator } from "@views/netobserv"
+import { netflowPage, overviewSelectors, querySumSelectors, colSelectors } from "@views/netflow-page"
+
+describe('(OCP-68246 Network_Observability) FlowRTT test', { tags: ['Network_Observability'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogin(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+
+        Operator.install()
+        cy.checkStorageClass(this)
+        Operator.createFlowcollector("FlowRTT")
+    })
+
+    beforeEach('any flowRTT test', function () {
+        netflowPage.visit()
+    })
+
+    it("(OCP-68246, aramesha, Network_Observability) Verify flowRTT panels", function () {
+        // to reduce flakes restore default panels first time it comes to overview page
+        cy.openPanelsModal();
+        cy.get(overviewSelectors.panelsModal).contains('Restore default panels').click();
+        cy.get(overviewSelectors.panelsModal).contains('Save').click();
+        netflowPage.waitForLokiQuery()
+
+        // verify Query Summary stats for flowRTT
+        cy.get(querySumSelectors.avgRTT).should('exist').then(avgRTT => {
+            cy.checkQuerySummary(avgRTT)
+        })
+
+        // verify default flowRTT panels are visible
+        cy.checkPanel(overviewSelectors.defaultFlowRTTPanels)
+        cy.checkPanelsNum(5);
+
+        // verify all relevant panels are listed
+        cy.openPanelsModal()
+        cy.checkPopupItems(overviewSelectors.panelsModal, overviewSelectors.manageFlowRTTPanelsList);
+
+        // select all panels and verify they are rendered
+        cy.get(overviewSelectors.panelsModal).contains('Select all').click();
+        cy.get(overviewSelectors.panelsModal).contains('Save').click();
+        netflowPage.waitForLokiQuery()
+        cy.checkPanelsNum(9);
+
+        netflowPage.waitForLokiQuery()
+        cy.checkPanel(overviewSelectors.allFlowRTTPanels)
+    })
+
+    it("(OCP-68246, aramesha, Network_Observability) Verify default flowRTT column", function () {
+        cy.get('#tabs-container li:nth-child(2)').click()
+        cy.byTestID("table-composable").should('exist')
+        netflowPage.stopAutoRefresh()
+
+        // verify default FowRTT column
+        cy.byTestID('table-composable').should('exist').within(() => {
+            cy.get(colSelectors.flowRTT).should('exist')
+        })
+    })
+
+    afterEach("test", function () {
+        netflowPage.resetClearFilters()
+    })
+
+    after("Delete flowcollector", function () {
+        Operator.deleteFlowCollector()
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+    })
+})

web/cypress/integration-tests/netflow_zone_multiCluster.cy.ts
@@ -1,0 +1,91 @@
+import { colSelectors, netflowPage, topologyPage, topologySelectors, setupTopologyViewWithNamespaceFilter } from "@views/netflow-page"
+import { Operator } from "@views/netobserv"
+
+function getTopologyScopeURL(scope: string): string {
+    return `**/flow/metrics**aggregateBy=${scope}*`
+}
+
+describe('Netflow Zone and multiCluster test', { tags: ['Network_Observability'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogin(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+
+        Operator.install()
+        cy.checkStorageClass(this)
+        Operator.createFlowcollector("ZonesAndMultiCluster")
+    })
+
+    beforeEach('any netflow zone and multiCluster test', function () {
+        netflowPage.visit()
+    })
+
+    it("(OCP-71525, aramesha, Network_Observability) should validate zone/multiCluster columns", function () {
+        cy.get('#tabs-container li:nth-child(2)').click()
+        cy.byTestID("table-composable").should('exist')
+
+        cy.openColumnsModal().then(col => {
+            cy.get(colSelectors.columnsModal).should('be.visible')
+            // Check zone columns
+            cy.get('#SrcZone').check()
+            cy.get('#DstZone').check()
+
+            // Check multiCluster column
+            cy.get('#ClusterName').check()
+            cy.byTestID(colSelectors.save).click()
+        })
+
+        cy.byTestID('table-composable').should('exist').within(() => {
+            // Verify zone column
+            cy.get(colSelectors.srcZone).should('exist')
+            cy.get(colSelectors.dstZone).should('exist')
+
+            // Verify multiCluster column
+            cy.get(colSelectors.clusterName).should('exist')
+        })
+    })
+
+    it("(OCP-71524, aramesha, Network_Observability) should verify zone/cluster scope topology", function () {
+        setupTopologyViewWithNamespaceFilter()
+        // Verify Zone scope
+        var scope = "zone"
+        cy.intercept('GET', getTopologyScopeURL(scope), {
+            fixture: 'flowmetrics/zone.json'
+        }).as('matchedUrl')
+
+        topologyPage.selectScopeGroup(scope, null)
+        cy.wait('@matchedUrl').then(({ response }) => {
+            expect(response.statusCode).to.eq(200)
+        })
+        topologyPage.isViewRendered()
+
+        // verify number of edges and nodes.
+        cy.get('#drawer ' + topologySelectors.edge).should('have.length', 6)
+        cy.get('#drawer ' + topologySelectors.node).should('have.length', 4)
+
+        // Verify Cluster scope
+        scope = "cluster"
+        cy.intercept('GET', getTopologyScopeURL(scope), {
+            fixture: 'flowmetrics/cluster.json'
+        }).as('matchedUrl')
+
+        topologyPage.selectScopeGroup(scope, null)
+        cy.wait('@matchedUrl').then(({ response }) => {
+            expect(response.statusCode).to.eq(200)
+        })
+        topologyPage.isViewRendered()
+
+        // verify number of edges and nodes.
+        cy.get('#drawer ' + topologySelectors.edge).should('have.length', 0)
+        cy.get('#drawer ' + topologySelectors.node).should('have.length', 1)
+    })
+
+    afterEach("test", function () {
+        netflowPage.resetClearFilters()
+    })
+
+    after("delete flowcollector and NetObs Operator", function () {
+        Operator.deleteFlowCollector()
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+    })
+})

memodi/netobserv-web-console

web/cypress/integration-tests/dns_tracking.cy.ts
@@ -1,0 +1,98 @@
+import { colSelectors, filterSelectors, netflowPage, overviewSelectors, querySumSelectors } from "@views/netflow-page"
+import { Operator, project } from "@views/netobserv"
+
+describe('(OCP-67087 Network_Observability) DNSTracking test', { tags: ['Network_Observability'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogin(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+
+        Operator.install()
+        cy.checkStorageClass(this)
+        Operator.createFlowcollector("DNSTracking")
+    })
+
+    beforeEach('any DNSTracking test', function () {
+        netflowPage.visit()
+    })
+
+    it("(OCP-67087, aramesha, Network_Observability) Verify DNSTracking panels and Query Summary", function () {
+        // verify default DNSTracking panels are visible
+        cy.checkPanel(overviewSelectors.defaultDNSTrackingPanels)
+        cy.checkPanelsNum(6);
+
+        // open panels modal and verify all relevant panels are listed
+        cy.openPanelsModal()
+        cy.checkPopupItems(overviewSelectors.panelsModal, overviewSelectors.manageDNSTrackingPanelsList);
+
+        // select all panels and verify they are rendered
+        cy.get(overviewSelectors.panelsModal).contains('Select all').click();
+        cy.get(overviewSelectors.panelsModal).contains('Save').click();
+        netflowPage.waitForLokiQuery()
+        cy.checkPanelsNum(11);
+
+        netflowPage.waitForLokiQuery()
+        cy.checkPanel(overviewSelectors.allDNSTrackingPanels)
+
+        // restore default panels and verify they are visible
+        cy.openPanelsModal();
+        cy.byTestID(overviewSelectors.resetDefault).click().byTestID(overviewSelectors.save).click()
+        netflowPage.waitForLokiQuery()
+        cy.checkPanel(overviewSelectors.defaultDNSTrackingPanels)
+        cy.checkPanelsNum(6);
+
+        // verify Query Summary stats for DNSTracking
+        cy.get(querySumSelectors.dnsAvg).should('exist').then(DNSAvg => {
+            cy.checkQuerySummary(DNSAvg)
+        })
+    })
+
+    it("(OCP-67087, aramesha) Validate DNSTracking columns and DNSName", function () {
+        cy.get('#tabs-container li:nth-child(2)').click()
+        cy.byTestID("table-composable").should('exist')
+        netflowPage.stopAutoRefresh()
+
+        // verify default DNS columns: DNS Latency and DNS Response Code
+        cy.byTestID('table-composable').should('exist').within(() => {
+            cy.get(colSelectors.dnsLatency).should('exist')
+            cy.get(colSelectors.dnsResponseCode).should('exist')
+        })
+
+        // add filter for dst_ns=netobserv and DNSName=loki
+        const dns_name = "loki.netobserv.svc.cluster"
+        cy.get(filterSelectors.filterInput).type("dst_namespace=" + project + '{enter}')
+        cy.get(filterSelectors.filterInput).type("dns_name=" + dns_name + '{enter}')
+
+        // select DNS Id and DNS Error columns
+        cy.openColumnsModal().then(col => {
+            cy.get(colSelectors.columnsModal).should('be.visible')
+            cy.get('#DNSId').check()
+            cy.get('#DNSErrNo').check()
+            cy.get('#DNSName').check()
+            cy.byTestID(colSelectors.save).click()
+        })
+
+        // verify they are visible in table view
+        cy.byTestID('table-composable').should('exist').within(() => {
+            cy.get(colSelectors.dnsId).should('exist')
+            cy.get(colSelectors.dnsError).should('exist')
+            cy.get(colSelectors.dnsName).should('exist')
+        })
+
+        // Verify DNSName column for all rows
+        cy.get('[data-test-td-column-id="DNSName"]').each((td) => {
+            expect(td).to.contain(`${dns_name}`)
+        })
+
+        netflowPage.clearAllFilters()
+    })
+
+    afterEach("test", function () {
+        netflowPage.resetClearFilters()
+    })
+
+    after("Delete flowcollector and DNS pods", function () {
+        Operator.deleteFlowCollector()
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+    })
+})

web/cypress/integration-tests/flowRTT.cy.ts
@@ -1,0 +1,68 @@
+import { colSelectors, netflowPage, overviewSelectors, querySumSelectors } from "@views/netflow-page"
+import { Operator } from "@views/netobserv"
+
+describe('(OCP-68246 Network_Observability) FlowRTT test', { tags: ['Network_Observability'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogin(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+
+        Operator.install()
+        cy.checkStorageClass(this)
+        Operator.createFlowcollector("FlowRTT")
+    })
+
+    beforeEach('any flowRTT test', function () {
+        netflowPage.visit()
+    })
+
+    it("(OCP-68246, aramesha, Network_Observability) Verify flowRTT panels", function () {
+        // to reduce flakes restore default panels first time it comes to overview page
+        cy.openPanelsModal();
+        cy.get(overviewSelectors.panelsModal).contains('Restore default panels').click();
+        cy.get(overviewSelectors.panelsModal).contains('Save').click();
+        netflowPage.waitForLokiQuery()
+
+        // verify Query Summary stats for flowRTT
+        cy.get(querySumSelectors.avgRTT).should('exist').then(avgRTT => {
+            cy.checkQuerySummary(avgRTT)
+        })
+
+        // verify default flowRTT panels are visible
+        cy.checkPanel(overviewSelectors.defaultFlowRTTPanels)
+        cy.checkPanelsNum(5);
+
+        // verify all relevant panels are listed
+        cy.openPanelsModal()
+        cy.checkPopupItems(overviewSelectors.panelsModal, overviewSelectors.manageFlowRTTPanelsList);
+
+        // select all panels and verify they are rendered
+        cy.get(overviewSelectors.panelsModal).contains('Select all').click();
+        cy.get(overviewSelectors.panelsModal).contains('Save').click();
+        netflowPage.waitForLokiQuery()
+        cy.checkPanelsNum(9);
+
+        netflowPage.waitForLokiQuery()
+        cy.checkPanel(overviewSelectors.allFlowRTTPanels)
+    })
+
+    it("(OCP-68246, aramesha, Network_Observability) Verify default flowRTT column", function () {
+        cy.get('#tabs-container li:nth-child(2)').click()
+        cy.byTestID("table-composable").should('exist')
+        netflowPage.stopAutoRefresh()
+
+        // verify default FowRTT column
+        cy.byTestID('table-composable').should('exist').within(() => {
+            cy.get(colSelectors.flowRTT).should('exist')
+        })
+    })
+
+    afterEach("test", function () {
+        netflowPage.resetClearFilters()
+    })
+
+    after("Delete flowcollector", function () {
+        Operator.deleteFlowCollector()
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+    })
+})

web/cypress/integration-tests/netflow_zone_multiCluster.cy.ts
@@ -1,0 +1,91 @@
+import { colSelectors, netflowPage, setupTopologyViewWithNamespaceFilter, topologyPage, topologySelectors } from "@views/netflow-page"
+import { Operator } from "@views/netobserv"
+
+function getTopologyScopeURL(scope: string): string {
+    return `**/flow/metrics**aggregateBy=${scope}*`
+}
+
+describe('Netflow Zone and multiCluster test', { tags: ['Network_Observability'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogin(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+
+        Operator.install()
+        cy.checkStorageClass(this)
+        Operator.createFlowcollector("ZonesAndMultiCluster")
+    })
+
+    beforeEach('any netflow zone and multiCluster test', function () {
+        netflowPage.visit()
+    })
+
+    it("(OCP-71525, aramesha, Network_Observability) should validate zone/multiCluster columns", function () {
+        cy.get('#tabs-container li:nth-child(2)').click()
+        cy.byTestID("table-composable").should('exist')
+
+        cy.openColumnsModal().then(col => {
+            cy.get(colSelectors.columnsModal).should('be.visible')
+            // Check zone columns
+            cy.get('#SrcZone').check()
+            cy.get('#DstZone').check()
+
+            // Check multiCluster column
+            cy.get('#ClusterName').check()
+            cy.byTestID(colSelectors.save).click()
+        })
+
+        cy.byTestID('table-composable').should('exist').within(() => {
+            // Verify zone column
+            cy.get(colSelectors.srcZone).should('exist')
+            cy.get(colSelectors.dstZone).should('exist')
+
+            // Verify multiCluster column
+            cy.get(colSelectors.clusterName).should('exist')
+        })
+    })
+
+    it("(OCP-71524, aramesha, Network_Observability) should verify zone/cluster scope topology", function () {
+        setupTopologyViewWithNamespaceFilter()
+        // Verify Zone scope
+        var scope = "zone"
+        cy.intercept('GET', getTopologyScopeURL(scope), {
+            fixture: 'flowmetrics/zone.json'
+        }).as('matchedUrl')
+
+        topologyPage.selectScopeGroup(scope, null)
+        cy.wait('@matchedUrl').then(({ response }) => {
+            expect(response.statusCode).to.eq(200)
+        })
+        topologyPage.isViewRendered()
+
+        // verify number of edges and nodes.
+        cy.get('#drawer ' + topologySelectors.edge).should('have.length', 6)
+        cy.get('#drawer ' + topologySelectors.node).should('have.length', 4)
+
+        // Verify Cluster scope
+        scope = "cluster"
+        cy.intercept('GET', getTopologyScopeURL(scope), {
+            fixture: 'flowmetrics/cluster.json'
+        }).as('matchedUrl')
+
+        topologyPage.selectScopeGroup(scope, null)
+        cy.wait('@matchedUrl').then(({ response }) => {
+            expect(response.statusCode).to.eq(200)
+        })
+        topologyPage.isViewRendered()
+
+        // verify number of edges and nodes.
+        cy.get('#drawer ' + topologySelectors.edge).should('have.length', 0)
+        cy.get('#drawer ' + topologySelectors.node).should('have.length', 1)
+    })
+
+    afterEach("test", function () {
+        netflowPage.resetClearFilters()
+    })
+
+    after("delete flowcollector and NetObs Operator", function () {
+        Operator.deleteFlowCollector()
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+    })
+})